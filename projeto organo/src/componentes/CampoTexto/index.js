226962a0639c72eddc48d0de80b6ddbcfbde9545
mv23c <100789090+mv23c@users.noreply.github.com>
2022-07-14 10:52:44 -0300

    Projeto organo

---
+++ projeto organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,20 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    let valor='Mário';
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor);
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+        </div>
+    );
+}
+
+export default CampoTexto;
